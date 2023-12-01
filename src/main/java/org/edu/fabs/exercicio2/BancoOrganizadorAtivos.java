19fde74e7bd35310bdace1f700553d67460bbc13
bianavic <bianavic@gmail.com>
2023-12-01 17:54:08 -0300

    refactor: exercicio 2 class name

--- src/main/java/org/edu/fabs/exercicio2/BancoOrganizadorAtivos.java
+++ src/main/java/org/edu/fabs/exercicio2/BancoOrganizadorAtivos.java
@@ -1,17 +1,16 @@
 package org.edu.fabs.exercicio2;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
-public class BancoOrganizadorAtivosResolucao {
+public class BancoOrganizadorAtivos {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> ativos = new ArrayList<>();
 
         //Entrada dos tipos de ativos
         int quantidadeAtivos = scanner.nextInt();
-        scanner.nextLine();
 
         // Entrada dos códigos dos ativos
         for (int i = 0; i < quantidadeAtivos; i++) {
@@ -20,13 +19,7 @@ public class BancoOrganizadorAtivosResolucao {
         }
 
         //TODO: Ordenar os ativos em ordem alfabética.
-        Collections.sort(ativos);
 
         //TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
-        for (String ativo : ativos) {
-            System.out.println(ativo);
-        }
-
     }
-
 }
--- src/main/java/org/edu/fabs/exercicio2/BancoOrganizadorAtivosResolvido.java
+++ src/main/java/org/edu/fabs/exercicio2/BancoOrganizadorAtivosResolvido.java
@@ -1,16 +1,17 @@
 package org.edu.fabs.exercicio2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class BancoOrganizadorAtivosExercicio {
-
+public class BancoOrganizadorAtivosResolvido {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> ativos = new ArrayList<>();
 
         //Entrada dos tipos de ativos
         int quantidadeAtivos = scanner.nextInt();
+        scanner.nextLine();
 
         // Entrada dos códigos dos ativos
         for (int i = 0; i < quantidadeAtivos; i++) {
@@ -19,7 +20,13 @@ public class BancoOrganizadorAtivosExercicio {
         }
 
         //TODO: Ordenar os ativos em ordem alfabética.
+        Collections.sort(ativos);
 
         //TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
+        for (String ativo : ativos) {
+            System.out.println(ativo);
+        }
+
     }
+
 }
