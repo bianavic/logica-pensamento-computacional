ad7cc6c8c8dfb93cb3baa4de06375765c40dc999
bianavic <bianavic@gmail.com>
2023-12-02 13:59:00 -0300

    resolucao desafio 4

---
+++ src/main/java/org/edu/fabs/desafio4/resolucao/Analise.java
@@ -0,0 +1,15 @@
+package org.edu.fabs.desafio4.resolucao;
+
+import java.util.Date;
+
+public class Analise {
+
+    Date data;
+    String descricao;
+
+    public Analise(Date data, String descricao) {
+        this.data = data;
+        this.descricao = descricao;
+    }
+
+}
